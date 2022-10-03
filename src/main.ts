94ae2a8edd7fb33d31934ac897d85ed7b89a2a98
DevNitro5 <98446102+DevNitro5@users.noreply.github.com>
2022-10-03 08:50:42 +0545

    implement demux

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,5 @@ export * as NumberUtils from "./utils/numbers";
 export * as Encoder from "./coder/encoder";
 export * as Decoder from "./coder/decoder";
 export * as Multiplexer from './plexer/multiplexer';
+export * as Demultiplexer from './plexer/demultiplexer';
 
